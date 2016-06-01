e1b561c3dd007d1bba39af6aaec0daf0c063807f
Britney Lovell <britneyklovell@gmail.com>
2016-05-31 20:10:56 -0700

    completed haiku

--- haiku.js
+++ haiku.js
@@ -1,28 +1,3 @@
-function createHaiku(structure){
-    console.log("this should log a haiku with the structure " + structure);
-}
+var haiku = require('./haiku_generator');
 
-module.exports = {
-  createHaiku: createHaiku,
-};
-
-
-
-// var fs = require("fs");
-// var cmudictFile = readCmudictFile('./cmudict.txt');
-
-// function readCmudictFile(file){
-//   return fs.readFileSync(file).toString();
-// }
-
-// function formatData(data){    
-//    var lines = data.toString().split("\n"),
-//        lineSplit
-//    lines.forEach(function(line){    
-//     lineSplit = line.split("  ");    
-//     console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]); 
-
-//   });   
-// }
-
-// formatData(cmudictFile);
+console.log(haiku.createHaiku([5,7,5]));
--- haiku_generator.js
+++ haiku_generator.js
@@ -1,22 +1,55 @@
-var haiku = require('./haiku');
-
-haiku.createHaiku([5,7,5]);
-
 var fs = require("fs");
 var cmudictFile = readCmudictFile('./cmudict.txt');
+
 
 function readCmudictFile(file){
   return fs.readFileSync(file).toString();
 }
 
-function formatData(data){    
-   var lines = data.toString().split("\n"),
-       lineSplit
-   lines.forEach(function(line){    
-    lineSplit = line.split("  ");    
-    console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]); 
+var countSyllables = function(phoneme) {
+	// Search for digits in phoneme. Return number of syllables.
 
-  });   
+	var found = phoneme.match(/\d/gi);
+	
+	if (found) {
+		return found.length;
+	} 
+	
+	return 0;
 }
 
-formatData(cmudictFile);
+
+function formatData(data){    
+   var lines = data.toString().split("\n");
+   var wordList = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]; 
+
+   lines.forEach(function(line){ 
+   		var word = line.split("  ")[0];
+   		var numberOfSyllables = countSyllables(line.split("  ")[1]);
+   		
+   		wordList[numberOfSyllables].push(word);
+    }); 
+
+  	return wordList; 
+}
+
+
+var wordList = formatData(cmudictFile);
+
+
+function createHaiku(structure){
+    var haiku = '';
+    
+    for (var i = 0; i < structure.length; i++) {
+    	var randomIndex = Math.floor(Math.random() * (wordList[structure[i]].length));
+    	haiku += wordList[structure[i]][randomIndex] +'\n';
+    };
+    
+    return haiku;
+}
+
+
+
+module.exports = {
+  createHaiku: createHaiku,
+};
